0ec3206b15353a96d505147124ed0940a559e0ad
EssiRoberto <essiroberto@gmail.com>
2026-08-17 15:27:52 +0100

    final commit

--- restart.sh
+++ restart.sh
@@ -4,9 +4,6 @@ if [ -f "$PID_FILE" ]; then
   PID=$(cat "$PID_FILE")
   kill -15 "$PID"
   rm "$PID_FILE"
-  echo "Process $PID stopped"
-else
-  echo "No PID file found"
 fi
 sleep 60 &
 NEW_PID=$!
